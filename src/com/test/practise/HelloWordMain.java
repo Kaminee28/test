69cf28ba8be5ad49115b9485323dfe3664d8f05e
kbodakhe@greatwolf.com <kbodakhe@greatwolf.com>
2023-06-01 06:48:30 +0530

    initial commit

---
+++ src/com/test/practise/HelloWordMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.test.practise;
+
+/**
+ * @author Admin
+ *
+ */
+public class HelloWordMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
