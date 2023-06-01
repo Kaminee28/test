1a24ceb56120a880d4431c2edf06aef7a7edb3a8
Admin <Admin@DESKTOP-AUIU8RR>
2023-06-01 07:10:44 +0530

    commit test 2

--- src/com/test/practise/HelloWordMain.java
+++ src/com/test/practise/HelloWordMain.java
@@ -15,6 +15,8 @@ public class HelloWordMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("https://github.com/Kaminee28/test.git");
+System.out.println("Updated kaminee program on 31may23");
+
 	}
 
 }
